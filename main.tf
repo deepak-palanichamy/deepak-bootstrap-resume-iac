8c008a18b972bac8f3fbafb30bde8762e778a584
Deepak Palanisamy <deepakpalanichamy@gmail.com>
2024-05-16 17:04:28 +0530

    update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,10 @@ provider "aws" {
   profile = "breezeware-demo-AWSAdministratorAccess-736932228178"
 }
 
-module "static_site" {
-  source                           = "./modules/s3_site"
-  sample_terraform_bucket_name_var = "2cca93cd-281e-4570-a9e3-20b4fe5e2a56"
+# module "static_site" {
+#   source                           = "./modules/s3_site"
+#   sample_terraform_bucket_name_var = "2cca93cd-281e-4570-a9e3-20b4fe5e2a56"
+# }
+
+resource "aws_s3_bucket" "demo_bucket" {
 }
